3195754bccd5edcd9b9e804edce074be69d6b037
IaraStevani <iarastevanialves@gmail.com>
2025-11-15 22:44:23 -0300

    Configurando realatório mochawesome no projeto Cypress BDD Cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,18 @@ const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-pr
 const {createEsbuildPlugin} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
+  projectId: '15qp1h',
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts:true,
+      reportTitle: 'Bootcamp QAzando - Cypress BDD Cucumber',
+      reportPageTitle: 'Bootcamp QAzando - Cypress BDD Cucumber',
+    },
     baseUrl:'http://automationpratice.com.br',
     specPattern: "**/*.feature",    
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       addCucumberPreprocessorPlugin(on, config);
 
       on(
